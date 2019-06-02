eff68472b6ef31bef0c9a3c3c1bc84f864828a68
mjcumming <mike@4831.com>
2019-06-02 07:55:26 -0500

    ver

--- bridge/__init__.py
+++ bridge/__init__.py
@@ -1,2 +1,2 @@
-name="ISY Homie Bridge"
-__version__ = "0.0.1"
+name="ISY Homie3 Bridge"
+__version__ = "0.0.2"
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setuptools.setup(name='ISY994 to Homie 3 Bridge',
-      version='0.0.1',
+setuptools.setup(name='ISY994 Homie3 Bridge',
+      version='0.0.2',
       description='Homie 3 for Universal Devices ISY994',
       author='Michael Cumming',
       author_email='mike@4831.com',
@@ -15,7 +15,6 @@ setuptools.setup(name='ISY994 to Homie 3 Bridge',
       keywords = ['HOMIE','MQTT','ISY99','Universal Devices'],  
       packages=setuptools.find_packages(exclude=("test.py",)),
       classifiers=[
-            "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
       ],      
