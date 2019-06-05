6bf8c7438fe631364586a580983cbb8940ada640
mjcumming <mike@4831.com>
2019-06-05 07:21:09 -0500

    misc fixes

--- bridge/bridge.py
+++ bridge/bridge.py
@@ -49,24 +49,22 @@ class Bridge (object):
         self.homie_settings = homie_settings
         self.mqtt_settings = mqtt_settings
 
-        self.gotavar = False
-
         self.controller = Controller(address=address,port=None,username=username,password=password,use_https=False,event_handler=self._isy_event_handler)
 
     def _isy_event_handler(self,container,item,event,*args):
-        logger.info ('Event {} from {}: {} {}'.format(event,container.container_type,item.name,args))
+        logger.warn ('Event {} from {}: {} {}'.format(event,container.container_type,item.name,args))
 
         if container.container_type == 'Device':
-            #self._device_event_handler (item,event,args)
+            self._device_event_handler (item,event,args)
             pass
         elif container.container_type == 'Scene':
-            #self._scene_event_handler (item,event,args)
+            self._scene_event_handler (item,event,args)
             pass
         elif container.container_type == 'Variable':
             self._variable_event_handler (item,event,args)
             pass
         elif container.container_type == 'Program':
-            #self._program_event_handler (item,event,args)
+            self._program_event_handler (item,event,args)
             pass
         elif container.container_type == 'Controller':
             self._container_event_handler (item,event,args)
@@ -92,13 +90,8 @@ class Bridge (object):
             scene = Scene (device,self.homie_settings,self.mqtt_settings)
 
     def _variable_event_handler(self,device,event,*args):
-        if self.gotavar:
-            return
-        print ('Variable event !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ',device.name,event)
-
         if event == 'add':
             variable = Variable (device,self.homie_settings,self.mqtt_settings)
-            self.gotavar = True
 
     def _program_event_handler(self,device,event,*args):
         #print ('device event',device.name,event)
