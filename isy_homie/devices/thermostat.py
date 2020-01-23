d3433a1dd66582e1e50bec60ad9073b3254a30c6
Patrick Nielsen <pat@burnttech.com>
2020-01-23 15:16:43 -0600

    Adding Thermostat System Status

--- isy_homie/devices/thermostat.py
+++ isy_homie/devices/thermostat.py
@@ -3,22 +3,21 @@
 from homie.device_thermostat import Device_Thermostat
 from .base import Base
 
-
 THERMOSTAT_SETTINGS = {
-    'current_temperature' : 70,
-    'current_humidity' : 30,
-    'cool_setpoint' : 70,
-    'max_cool_setpoint' : 90,
-    'min_cool_setpoint' : 60,
-    'heat_setpoint' : 70,
-    'max_heat_setpoint' : 90,
-    'min_heat_setpoint' : 60,    
-    'fan_mode' : 'On',
-    'fan_modes' : ['On','Auto'],
-    'system_mode' : 'Off',
-    'system_modes' : ['Off','Heat','Cool','Auto'],
-    'system_status' : 'Off',
-    'units' : 'F',
+    'current_temperature': 70,
+    'current_humidity': 30,
+    'cool_setpoint': 70,
+    'max_cool_setpoint': 90,
+    'min_cool_setpoint': 60,
+    'heat_setpoint': 70,
+    'max_heat_setpoint': 90,
+    'min_heat_setpoint': 60,
+    'fan_mode': 'On',
+    'fan_modes': ['On', 'Auto'],
+    'system_mode': 'Off',
+    'system_modes': ['Off', 'Heat', 'Cool', 'Auto'],
+    'system_status': ['Idle', 'Heating', 'Cooling'],
+    'units': 'F',
 }
 
 props = [
@@ -27,25 +26,27 @@ props = [
     'heatsetpoint',
     'coolsetpoint',
     'systemmode',
+    'systemstatus',
     'fanmode'
 ]
 
-class Thermostat(Base,Device_Thermostat):
+
+class Thermostat(Base, Device_Thermostat):
 
     def __init__(self, isy_device=None,homie_settings=None, mqtt_settings=None):
 
-        Base.__init__ (self,isy_device)
-        Device_Thermostat.__init__ (self,self.get_homie_device_id(), isy_device.name, homie_settings, mqtt_settings)
-    
+        Base.__init__(self, isy_device)
+        Device_Thermostat.__init__(self, self.get_homie_device_id(), isy_device.name, homie_settings, mqtt_settings)
+
         for isy_prop in props:
             iprop_value = self.isy_device.get_property(isy_prop)
             if iprop_value is not None:
-                self.property_change(isy_prop,iprop_value)
-   
-    def get_homie_device_id (self):
+                self.property_change(isy_prop, iprop_value)
+
+    def get_homie_device_id(self):
         return 'thermostat-' + Base.get_homie_device_id(self)
 
-    def property_change(self,property_,value):
+    def property_change(self, property_, value):
         if property_ == 'temperature':
             self.update_current_temperature(value)
         elif property_ == 'humidity':
@@ -56,22 +57,27 @@ class Thermostat(Base,Device_Thermostat):
             self.update_cool_setpoint(value)
         elif property_ == 'systemmode':
             self.update_system_mode(value)
+        elif property_ == 'systemstatus':
+            self.update_system_status(value)
         elif property_ == 'fanmode':
             self.update_fan_mode(value)
 
-        Base.property_change (self,property_,value)
+        Base.property_change(self, property_, value)
 
-    def set_heat_setpoint(self,value):
+    def set_heat_setpoint(self, value):
         self.isy_device.set_heatsetpoint(value)
 
-    def set_cool_setpoint(self,value):
+    def set_cool_setpoint(self, value):
         self.isy_device.set_coolsetpoint(value)
-        
-    def set_system_mode(self,value):
+
+    def set_system_mode(self, value):
         self.isy_device.setmode(value)
-        
-    def set_fan_mode(self,value):
+
+    def set_system_status(self, value):
+        self.isy_device.setstatus(value)
+
+    def set_fan_mode(self, value):
         pass
-        
-    def set_hold_mode(self,value):
+
+    def set_hold_mode(self, value):
         pass
